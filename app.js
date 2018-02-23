62c895bac35535ef1dd72d116853fcd1e98d4b1c
Rocket-Hyun <rockethyun@gmail.com>
2018-02-23 14:56:09 +0900

    body parser urlencode request added

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 var firebase = require("firebase");
 var cache = require("./lib/memoryCache");
 
